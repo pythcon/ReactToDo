d48e0102a838e20cceaafa7ac60ea616cb255499
Todd Murphy <tam1998@comcast.net>
2019-10-15 16:41:59 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import from './components/Todos';
+
+function App() {
+  return (
+    <div className="App">
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
