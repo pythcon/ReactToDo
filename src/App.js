e5f508d1c5c1be1da8d42acc450e7ec95a118c4d
Todd Murphy <tam1998@comcast.net>
2019-10-15 18:02:53 -0400

    fixed all problems and converted function -> class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import from './components/Todos';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import '../App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+class Todos extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Todos</h1>
+      </div>
+    );
+  }
 }
 
 export default Todos;
